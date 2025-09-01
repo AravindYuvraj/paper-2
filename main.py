a5896cc752a8f5782afdcea42789fc0e4cadd4d5
Aravind <aravind98761234@gmail.com>
2025-09-01 18:25:57 +0530

    working-1

--- main.py
+++ main.py
@@ -3,79 +3,62 @@
 # It should allow users to manage their wallets, maintain transaction history, and perform secure transactions(simulate money transfer between users).
 
 
-from typing import Annotated
+from typing import Annotated, Union
 
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from pydantic import BaseModel
 from datetime import datetime
 from sqlmodel import Relationship
-
-
-
-# Relationship class to link between the user and transactions
-class UserTransactionLink(SQLModel, table=True):
-    user_id: int = Field(foreign_key="user.id", primary_key=True)
-    transaction_id: int = Field(foreign_key="transaction.id", primary_key=True)
+from typing import List, Optional
 #-------------------------
-# Database Models (TABLES)
-#-------------------------
+
+
+# DATABASE TABLES
 class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Union[int, None] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
     email: str = Field(index=True, unique=True)
-    # password: str
     phone_number: str = Field(index=True, unique=True)
     balance: float = Field(default=0.0)
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
 
-    transactions: list["Transaction"] = Relationship(back_populates="user", link_model=UserTransactionLink)
-
 class Transaction(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
+    user_id: int = Field(foreign_key="User.id")
     # Transaction type: one of 'CREDIT', 'DEBIT', 'TRANSFER_IN', 'TRANSFER_OUT'
     transaction_type: str
-    recipient_user_id: int = Field(foreign_key="user.id")
+    recipient_user_id: int = Field(foreign_key="User.id")
     amount: float
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
 
-#------------------
-# REQUEST MODELS
-#-------------------
-class UserCreate(BaseModel):
-    username: str
+
+# REQUEST RESPONSE MODELS
+class UserBase(SQLModel):
+    username: str = Field(index=True)
+    email: str = Field(index=True)
+    phone_number: str = Field(index=True)
+    balance: float = Field(default=0.0)
+
+class UserCreate(UserBase):
+    username:str
     email: str
     phone_number: str
 
-class TransactionRequest(BaseModel):
-    user_id: int
-    transaction_type: str
-    recipient_user_id: int
-    amount: float
-    balance: float
+class UserPublic(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
 
-    class Config:
-        orm_mode = True
+class UserUpdate(UserBase):
+    username: Optional[str] = None
+    phone_number: Optional[str] = None
 
-class TransactionResponse(BaseModel):
-    user_id: int
-    transaction_type: str
-    recipient_user_id: int
-    amount: float
-
-    class Config:
-        orm_mode = True
-
-        
 
 #----------------
-# DATABASE SETUP
-#----------------
-
-sqlite_file_name = "transaction.db"
+sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
@@ -92,29 +75,73 @@ def get_session():
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
-
 app = FastAPI()
 
-#------------------
-# API ENDPOINTS
-#-----------------
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Welcome to the Digital Wallet API"}
 
-
-# CRUD for User
-
-# 1. Create
-@app.post("/users/", response_model=UserResponse)
+@app.post("/users/", response_model=UserPublic)
 def create_user(user: UserCreate, session: SessionDep):
+    db_user = User.model_validate(user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@app.get("/users/", response_model=list[UserPublic])
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@app.get("/users/{user_id}", response_model=UserPublic)
+def read_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+# PUT /users/{user_id} update user username and email
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: UserUpdate, session: SessionDep):
+    db_user = session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_user.username = user.username or db_user.username
+    db_user.email = user.email or db_user.email
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+# GET /wallet/{user_id}/balance
+@app.get("/wallet/{user_id}/balance")
+def get_wallet_balance(user_id: int, session: SessionDep):
+    user = session.exec(select(User).where(User.id == user_id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user_id": user.id,"balance": user.balance,"last_updated": user.updated_at}
+
+
+# POST /wallet/{user_id}/add-money
+@app.post("/wallet/{user_id}/add-money")
+def add_money_to_wallet(user_id: int, amount: float,description: str, session: SessionDep):
+    user = session.exec(select(User).where(User.id == user_id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.balance += amount
     session.add(user)
     session.commit()
     session.refresh(user)
-    return user
-
+    return {
+  "user_id": user.id, "amount": amount, "new_balance": user.balance, "transaction_type": "CREDIT"
+}
